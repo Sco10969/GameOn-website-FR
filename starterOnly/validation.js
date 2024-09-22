921f8c0991c6554b10ba02c5d2ee3a455b1b1095
Sco10969 <tommy2909@hotmail.fr>
2024-09-22 14:58:59 +0200

    🎨 Mentorat Session

--- starterOnly/validation.js
+++ starterOnly/validation.js
@@ -9,141 +9,159 @@
  const locationInputs = document.querySelectorAll("input[name='location']");
  const checkboxInputs = document.querySelectorAll("input[type='checkbox']");
 
- const validateInputChars = {
-    validationFunction: (inputElement) => /^[a-zA-Z]+$/.test(inputElement.value.trim()),
-    errorMessage: "Ne doit contenir que des lettres."
-}
+ const validatorNotEmpty = {
+     validationFunction: (inputElement) => inputElement.value.trim() !== "",
+     errorMessage: "Champ requis."
+ };
 
- const validateInputLength = {
+ const validatorInputChars = {
+     validationFunction: (inputElement) => /^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:[-\s][A-Za-zÀ-ÖØ-öø-ÿ]+)*$/.test(inputElement.value.trim()),
+     errorMessage: "Ne doit contenir que des lettres."
+ }
+
+ const validatorInputLength = {
      validationFunction: (inputElement) => inputElement.value.trim().length >= 2,
      errorMessage: "Doit contenir au moins 2 caractères."
  }
 
- const validateInputName = [
-     validateInputChars,
-     validateInputLength
- ];
-
- const validateEmailFormat = {
+ const validatorEmailFormat = {
      validationFunction: (inputElement) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputElement.value.trim()),
      errorMessage: "Veuillez saisir une adresse email valide."
  };
 
- const validateBirthdate = {
-     validationFunction: (inputElement) => inputElement.value.trim() !== "",
-     errorMessage: "Vous devez entrer votre date de naissance."
+ const validatorQuantity = {
+     validationFunction: (inputElement) => {
+         const value = Number(inputElement.value);
+         return value >= 0 && value <= 99;
+     },
+     errorMessage: "Veuillez entrer un nombre valide."
  };
 
-//  const validateBirthdate = {
-//     validationFunction: (inputElement) => {
-//         const birthdate = new Date(inputElement.value);
-//     const today = new Date();
-//     let age = today.getFullYear() - birthdate.getFullYear();
-//     const monthDifference = today.getMonth() - birthdate.getMonth();
-
-//     if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthdate.getDate())) {
-//         age--;
-//     }
-
-//     return age >= 18;
-// },
-// errorMessage: "Vous devez avoir au moins 18 ans."
-// };
-
-const validateQuantity = {
-    validationFunction: (inputElement) => {
-        if (inputElement.value.trim() === "") {
-            validateQuantity.errorMessage = "Veuillez remplir ce champ.";
-            return false;
-        }
-        const value = Number(inputElement.value);
-        validateQuantity.errorMessage = "Veuillez entrer un nombre valide.";
-        return value >= 0 && value <= 99;
-    },
-};
-
- const validateLocation = {
-     validationFunction: () => Array.from(locationInputs).some(input => input.checked),
+ const validatorLocation = {
+     validationFunction: (inputElements) => Array.from(inputElements).some(input => input.checked),
      errorMessage: "Veuillez sélectionner un tournoi."
  };
 
- const validateCheckboxAccept = {
-     validationFunction: (inputElements) => inputElements[0].checked,
+ const validatorCheckboxAccept = {
+     validationFunction: (inputElement) => inputElement.checked,
      errorMessage: "Vous devez accepter les conditions d'utilisation."
  };
 
+ const nameValidators = [
+     validatorNotEmpty,
+     validatorInputChars,
+     validatorInputLength
+ ];
+
+ //  const validateBirthdate = {
+ //     validationFunction: (inputElement) => {
+ //         const birthdate = new Date(inputElement.value);
+ //     const today = new Date();
+ //     let age = today.getFullYear() - birthdate.getFullYear();
+ //     const monthDifference = today.getMonth() - birthdate.getMonth();
+
+ //     if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthdate.getDate())) {
+ //         age--;
+ //     }
+
+ //     return age >= 18;
+ // },
+ // errorMessage: "Vous devez avoir au moins 18 ans."
+ // };
+
  // Tableau des inputs à valider
- const inputsToValidate = [
-     {
+ const inputsToValidate = [{
          element: firstNameInput,
-         validations: validateInputName
+         validations: nameValidators
      },
      {
          element: lastNameInput,
-         validations: validateInputName
+         validations: nameValidators
      },
      {
          element: emailInput,
-         validations: [validateEmailFormat]
+         validations: [validatorEmailFormat]
      },
      {
          element: birthdateInput,
-         validations: [validateBirthdate]
+         validations: [validatorNotEmpty]
      },
      {
          element: quantityInput,
-         validations: [validateQuantity]
+         validations: [validatorNotEmpty, validatorQuantity]
      },
      {
          element: locationInputs,
-         validations: [validateLocation]
+         validations: [validatorLocation],
+         test: "click"
      },
      {
-         element: checkboxInputs,
-         validations: [validateCheckboxAccept]
+         element: checkboxInputs[0],
+         validations: [validatorCheckboxAccept],
+         test: "click"
      }
  ];
 
-// Fonction de validation d'un input
-function validateInput(inputElement, validations) {
-    let isValid = true;
-    const formDataElement = inputElement instanceof NodeList 
-        ? inputElement[0].closest(".formData") 
-        : inputElement.closest(".formData");
+ // Fonction de validation d'un input
+ function validateInput(inputElement, validations) {
+     let isValid = true;
+     const formDataElement = inputElement instanceof NodeList ?
+         inputElement[0].closest(".formData") :
+         inputElement.closest(".formData");
 
-    validations.forEach(validation => {
-        if (!validation.validationFunction(inputElement)) {
-            formDataElement.setAttribute("data-error", validation.errorMessage);
-            formDataElement.setAttribute("data-error-visible", "true");
-            isValid = false;
-        }
-    });
+     validations.forEach(validation => {
+         if (!validation.validationFunction(inputElement)) {
+             formDataElement.setAttribute("data-error", validation.errorMessage);
+             formDataElement.setAttribute("data-error-visible", "true");
+             isValid = false;
+         }
+     });
 
-    if (isValid) {
-        formDataElement.removeAttribute("data-error");
-        formDataElement.removeAttribute("data-error-visible");
-    }
+     if (isValid) {
+         formDataElement.removeAttribute("data-error");
+         formDataElement.removeAttribute("data-error-visible");
+     }
 
-    return isValid;
-}
- // TODO : Validation en temps réel
+     return isValid;
+ }
+
+ // Validation en temps réel
+ inputsToValidate.forEach(({
+     element,
+     validations,
+     test
+ }) => {
+     if (element instanceof NodeList) {
+         [...element].forEach(el => {
+             el.addEventListener(test ?? "blur", () => {
+                 validateInput(element, validations);
+             });
+         });
+     } else {
+         element.addEventListener(test ?? "blur", () => {
+             validateInput(element, validations);
+         });
+     }
+ })
 
  // Validation du formulaire lors de la soumission
  form.addEventListener("submit", function (event) {
-    event.preventDefault();
+     event.preventDefault();
 
-    let isFormValid = true;
+     let isFormValid = true;
 
-    // Validation de chaque input
-    inputsToValidate.forEach(({ element, validations }) => {
-        const valid = validateInput(element, validations);
-        if (!valid) {
-            isFormValid = false;
-        }
-    });
+     // Validation de chaque input
+     inputsToValidate.forEach(({
+         element,
+         validations
+     }) => {
+         const valid = validateInput(element, validations);
+         if (!valid) {
+             isFormValid = false;
+         }
+     });
 
-    if (isFormValid) {
-        // TODO : message de confirmation à mettre en place
-        form.submit(); // Soumet le formulaire si tout est valide
-    }
-});
+     if (isFormValid) {
+         // TODO : message de confirmation à mettre en place
+     }
+ });
